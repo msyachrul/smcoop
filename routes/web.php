57a1082b834ac6a96e958244540fb0515d4b15e2
Muhammad Syachrul <abdisyachrul@gmail.com>
2018-03-05 09:53:46 +0700

    Grouping route

--- routes/web.php
+++ routes/web.php
@@ -30,42 +30,50 @@ Route::get('/blank', function() {
 	return view('blank');
 });
 
-
-
-Route::group(['middleware' => ['web']], function() {
   // Anggota
-  Route::resource('anggota','anggotaController');
-  Route::post('anggota/daftar','anggotaController@tambah');
-  Route::post('anggota/edit','anggotaController@update');
-  Route::post('anggota/hapus','anggotaController@hapus');
+  Route::prefix('anggota')->group(function(){
+    Route::get('/','anggotaController@index');
+    Route::post('/daftar','anggotaController@tambah');
+    Route::post('/edit','anggotaController@update');
+    Route::post('/hapus','anggotaController@hapus');
+  });
 
   // Barang
-  Route::resource('barang','barangController');
-  Route::post('barang/daftar','barangController@tambah');
-  Route::post('barang/edit', 'barangController@update');
-  Route::post('barang/hapus','barangController@hapus');
+  Route::prefix('barang')->group(function(){
+    Route::get('/','barangController@index');
+    Route::post('/daftar','barangController@tambah');
+    Route::post('/edit', 'barangController@update');
+    Route::post('/hapus','barangController@hapus');
+  });
 
   // Pembelian
-  Route::get('pembelian', 'pembelianController@tampil');
-  Route::get('pembelian/autocomplete', 'pembelianController@autocomplete');
-  Route::post('pembelian/input', 'pembelianController@input');
-  Route::post('pembelian/edit', 'pembelianController@update');
-  Route::post('pembelian/hapus', 'pembelianController@hapus');
-  Route::post('pembelian/cari', 'pembelianController@cari');
+  Route::prefix('pembelian')->group(function(){
+    Route::get('pembelian', 'pembelianController@tampil');
+    Route::get('pembelian/autocomplete', 'pembelianController@autocomplete');
+    Route::post('pembelian/input', 'pembelianController@input');
+    Route::post('pembelian/edit', 'pembelianController@update');
+    Route::post('pembelian/hapus', 'pembelianController@hapus');
+    Route::post('pembelian/cari', 'pembelianController@cari');
+  });
 
   // Penjualan
-  Route::get('penjualan', 'penjualanController@index');
-  Route::get('penjualan/input', 'penjualanController@inputPenjualan');
-  Route::get('penjualan/input/anggota/autocomplete', 'penjualanController@autocompleteAnggota');
-  Route::get('penjualan/input/barang/autocomplete', 'penjualanController@autocompleteBarang');
-  Route::post('penjualan/input/barang/tambah', 'penjualanController@inputBarang');
-  Route::post('penjualan/input/transaksi', 'penjualanController@inputTransaksi');
-  Route::post('penjualan/input/barang/hapus', 'penjualanController@hapusTmpBarang');
-  Route::post('penjualan/input/barang/cek', 'penjualanController@cek');
-  Route::post('penjualan/batal', 'penjualanController@batal');
-  Route::get('penjualan/{no}', 'penjualanController@detail');
-  Route::get('penjualan/edit/{no}', 'penjualanController@edit');
-  Route::post('penjualan/edit/barang/tambah', 'penjualanController@tambahBarang');
-  Route::post('penjualan/edit/barang/hapus', 'penjualanController@hapusBarang');
-  Route::get('penjualan/hapus/{no}','penjualanController@hapusTransaksi');
-});
+  Route::prefix('penjualan')->group(function(){
+    Route::get('/', 'penjualanController@index');
+    Route::prefix('input')->group(function(){
+      Route::get('/', 'penjualanController@inputPenjualan');
+      Route::get('/anggota/autocomplete', 'penjualanController@autocompleteAnggota');
+      Route::get('/barang/autocomplete', 'penjualanController@autocompleteBarang');
+      Route::post('/barang/tambah', 'penjualanController@inputBarang');
+      Route::post('/transaksi', 'penjualanController@inputTransaksi');
+      Route::post('/barang/hapus', 'penjualanController@hapusTmpBarang');
+      Route::post('/barang/cek', 'penjualanController@cek');
+    });
+    Route::post('/batal', 'penjualanController@batal');
+    Route::get('/{no}', 'penjualanController@detail');
+    Route::prefix('edit')->group(function(){
+      Route::get('/{no}', 'penjualanController@edit');
+      Route::post('/barang/tambah', 'penjualanController@tambahBarang');
+      Route::post('/barang/hapus', 'penjualanController@hapusBarang');
+    });
+    Route::get('/hapus/{no}','penjualanController@hapusTransaksi');
+  });
